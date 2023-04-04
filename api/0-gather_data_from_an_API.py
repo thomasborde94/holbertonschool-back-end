a81bae440a8f9c214994c0326c1d532503e1ffe4
Thomas Borde <thomas.borde94@hotmail.fr>
2023-04-04 01:44:23 -0700

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -20,21 +20,21 @@ if __name__ == "__main__":
     data_users = r_users.json()
 
     for i in data_todos:
-        #check how many total tasks there are for the given user id
+        # check how many total tasks there are for the given user id
         if i.get("userId") == int(argv[1]):
             TOTAL_NUMBER_OF_TASKS += 1
-            #check how many tasks he has done
+            # check how many tasks he has done
             if i.get("completed") is True:
                 NUMBER_OF_DONE_TASKS += 1
-                #appends the title of the completed task to a list
+                # appends the title of the completed task to a list
                 TASK_TITLE.append(i.get("title"))
 
-    #collect the name of the employee
+    # collect the name of the employee
     for i in data_users:
         if i.get("id") == int(argv[1]):
             employee = i.get("name")
 
-    #print result to the correct format
+    # print result to the correct format
     print("Employee {} is done with tasks({}/{}):".format(
         employee, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
     for titles in TASK_TITLE:
